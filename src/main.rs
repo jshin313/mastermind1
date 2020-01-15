d9ca7a3650faeb8a108349a1ab543350b086ec9a
Jacob Shin <jacobshin313@gmail.com>
2020-01-15 06:48:26 -0500

    Display number of right and wrong

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,12 @@ fn main() {
     let mut tries = 0;
 
     loop {
+
+        // Keeps track of right, almost right, and wrong guesses
+        let mut right = 0;
+        let mut almost = 0;
+        let mut wrong = 0;
+
         print!("Input your guess: ");
         io::stdout().flush().unwrap(); // Stdout has a buffer, so this the above print won't display without flushing
 
@@ -38,6 +44,7 @@ fn main() {
 
             if rand_digit == input_digit {
                 println!("{} <-- {}", input_digit, correct);
+                right+=1;
             }
             else
             {
@@ -47,18 +54,26 @@ fn main() {
                     let rand_digit = rand_number / u32::pow(10, j) % 10;
                     if rand_digit == input_digit {
                         println!("{} <-- {}", input_digit, sortof);
+                        almost += 1;
                         break;
                     }
 
                     // If no match found anywhere in the random number
                     if j == 5 {
                         println!("{} <-- {}", input_digit, incorrect);
+                        wrong += 1;
                     }
                 }
 
             }
 
         }
+
+        // Prints how many digits the user got right, almost right, and wrong
+        println!("");
+        println!("You guessed {} digits correctly in the right position", right);
+        println!("You guessed {} digits correctly, but not in the right position", almost);
+        println!("You guessed {} digits completely incorrectly (i.e. not in the randomly generated number)", wrong);
 
         // For debug purposes
         // println!("Random number: {}", rand_number);
